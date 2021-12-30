e2ae5695cbd65a22ebf3fd29d1a5d357c0c3c3b5
Dmitriy <paranin-dmitriy@mail.ru>
2021-12-30 16:35:30 +0300

    31

--- src/componentss/Profile/MyPosts/MyPosts.tsx
+++ src/componentss/Profile/MyPosts/MyPosts.tsx
@@ -11,12 +11,18 @@ type MyPostsPropsType = {
 const MyPosts = (props:MyPostsPropsType) => {
       let postsElements = props.posts.map((p) => <Post message={p.message} likesCount={p.likesCount}/>)
 
+    let newPostElement = React.createRef();
+
+    let addPost = () => {
+        let text = newPostElement.current.value;
+    }
+
     return (
         <div>
-            My posts
+            <h3>My posts</h3>
             <div>
-                <textarea></textarea>
-                <button>Add post</button>
+                <textarea ref={newPostElement}></textarea>
+                <button onClick={addPost}>Add post</button>
                 <button>Remove</button>
             </div>
             <div className={s.posts}>
